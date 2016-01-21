c10f752296383f74a001301f9263ec9fbb16879c
Haider Malik <haidermalik5044@gmail.com>
2016-01-20 21:31:14 -0800

    Added Tods Collections

--- server.js
+++ server.js
@@ -3,7 +3,28 @@
  */
 var express=require('express');
 var app=express();
-var PORT=process.env.PORT || 4000;
+var PORT=process.env.PORT || 3000;
+
+//---------Todos collection-------------
+var todos=[{
+    id:1,
+    description:'Sstartup owners manual Ch 1',
+    completed:false
+},{
+    id:2,
+    description:'Design Business Model Canvas',
+    completed:false
+},
+    {
+        id:3,
+        description:'Learn Database Section in Nodejs',
+        completed:true
+    }];
+
+// Get /todos
+app.get('/todos',function(req,res){
+res.json(todos);
+});
 
 app.get('/',function(req,res){
 
